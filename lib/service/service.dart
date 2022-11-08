02adf69085ddeccf19f4d8c5e3f6881109149595
Abu Bakar Nawaz <abnawaz14@gmail.com>
2022-11-09 00:58:26 +0400

    format service.dart file

--- lib/service/service.dart
+++ lib/service/service.dart
@@ -1,3 +1,3 @@
-//GENERATED BARREL FILE 
-export 'go_router_refresh_stream.dart'; 
-export 'internet_availability_config.dart'; 
+//GENERATED BARREL FILE
+export 'go_router_refresh_stream.dart';
+export 'internet_availability_config.dart';
