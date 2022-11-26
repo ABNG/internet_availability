84c04ef77c711dd12a1dda5992d1ebe252f13f0d
Abu Bakar Nawaz Ghumman <44497582+ABNG@users.noreply.github.com>
2022-11-26 18:40:09 +0400

    added back gesture option (#2)

--- lib/presentation/page/no_internet_page.dart
+++ lib/presentation/page/no_internet_page.dart
@@ -3,49 +3,56 @@ import 'package:internet_availability/service/internet_availability_config.dart'
 
 class NoInternetPage extends StatelessWidget {
   static const String routeName = '/no_internet';
-  const NoInternetPage({Key? key}) : super(key: key);
+  final bool enableBackGesture;
+  const NoInternetPage({Key? key, this.enableBackGesture = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Center(
-            child: Image.asset('assets/no_internet.jpg',
-                width: 320, height: 320, package: 'internet_availability'),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          IconButton(
-            onPressed: () async {
-              final messenger = ScaffoldMessenger.of(context);
-              messenger.showSnackBar(
-                const SnackBar(
-                  content: Text('internet availability checking...'),
-                ),
-              );
-              await InternetAvailabilityConfig.internetAvailabilityCubit
-                  .checkInternetAvailability();
-              InternetAvailabilityConfig.internetAvailabilityCubit.state
-                  .whenOrNull(none: () {
-                messenger.hideCurrentSnackBar();
+    return WillPopScope(
+      onWillPop: () {
+        return Future.value(enableBackGesture);
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Image.asset('assets/no_internet.jpg',
+                  width: 320, height: 320, package: 'internet_availability'),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            IconButton(
+              onPressed: () async {
+                final messenger = ScaffoldMessenger.of(context);
                 messenger.showSnackBar(
                   const SnackBar(
-                    content: Text('No internet available'),
+                    content: Text('internet availability checking...'),
                   ),
                 );
-              });
-            },
-            icon: const Icon(
-              Icons.refresh,
-              size: 32,
-            ),
-          )
-        ],
+                await InternetAvailabilityConfig.internetAvailabilityCubit
+                    .checkInternetAvailability();
+                InternetAvailabilityConfig.internetAvailabilityCubit.state
+                    .whenOrNull(none: () {
+                  messenger.hideCurrentSnackBar();
+                  messenger.showSnackBar(
+                    const SnackBar(
+                      content: Text('No internet available'),
+                    ),
+                  );
+                });
+              },
+              icon: const Icon(
+                Icons.refresh,
+                size: 32,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
